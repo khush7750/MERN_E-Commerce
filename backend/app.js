7d43b9dad412829ca4ab75b7a99ba4628859e8e9
khush7750 <meherkhushbant@gmail.com>
2021-01-29 00:54:19 +0530

    Initial commit

--- backend/app.js
+++ backend/app.js
@@ -24,6 +24,8 @@ mongoose
   })
   .then(() => {
     console.log("DB CONNECTED");
+  }).catch(err=>{
+    console.log(err)
   });
 
 //Middlewares
@@ -41,6 +43,7 @@ app.use("/api", paymentBRoutes);
 
 //PORT
 const port = process.env.PORT || 8000;
+
 
 //Starting a server
 app.listen(port, () => {
